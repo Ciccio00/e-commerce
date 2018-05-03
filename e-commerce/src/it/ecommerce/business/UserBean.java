b6f2081450dbf3011a7b3b18c3d7ada4ad5ab956
sebastiano panico <34926889+ninopanico@users.noreply.github.com>
2018-05-03 18:27:00 +0200

    EJB

---
+++ e-commerce/src/it/ecommerce/business/UserBean.java
@@ -0,0 +1,40 @@
+package it.ecommerce.business;
+
+import java.util.List;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.websocket.server.PathParam;
+
+import it.ecommerce.entity.User;
+
+@Stateless
+@LocalBean
+public class UserBean implements UserBeanLocal {
+	@PersistenceContext(unitName="webPersistence")
+	 EntityManager m;
+	public UserBean() {
+		
+	}
+	
+	public void aggiungiuser (User u) {
+		m.persist(u);
+	}
+	
+	public void aggiornauser (Long id) {
+		User u= getUserID(id);
+		 m.merge(u);
+	}
+	public void cancellauser (Long id ) {
+		 User u =  getUserID(id);
+		 m.remove(u);
+	}
+	public User getUserID(@PathParam("iduser")Long id) {
+		return m.find(User.class, id);
+	}
+	public List<User>  cercautente(){
+		return m.createQuery("SELECT u FROM User u").getResultList();
+	}
+}
---
+++ e-commerce/src/it/ecommerce/business/UserBeanLocal.java
@@ -0,0 +1,13 @@
+package it.ecommerce.business;
+
+import java.util.List;
+
+import it.ecommerce.entity.User;
+
+public interface UserBeanLocal {
+            public void aggiungiuser(User u);
+            public void aggiornauser (Long id);
+        	public void cancellauser (Long id );
+        	public User getUserID(Long id);
+        	public List<User> cercautente();
+}
